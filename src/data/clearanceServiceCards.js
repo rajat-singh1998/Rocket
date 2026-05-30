6be9d825a8d744af4b2521ab927b877757f346ab
rajat-singh1998 <rajatpathania947@gmail.com>
2026-05-30 12:05:11 +0530

    image change

--- src/data/clearanceServiceCards.js
+++ src/data/clearanceServiceCards.js
@@ -3,36 +3,36 @@ export const clearanceServiceCards = [
     title: "Mini Load",
     meta: "1 Cubic Yards | 0.8 M²",
     note: "Boot Of Small Car | Up To 8 Dustbin Bags",
-    image: "/images/rocket/mini_l.png"
+    image: "/images/rocket/Mini_Load.webp"
   },
   {
     title: "Small Load",
     meta: "2 Cubic Yards | 1.5 M²",
     note: "Boot Of Estate Car | 10-15 Dustbin Bags",
-    image: "/images/rocket/small_l.png"
+    image: "/images/rocket/Small_Load.webp"
   },
   {
     title: "Medium Load",
     meta: "4 Cubic Yards | 3.1 M²",
     note: "1 Midi Skip | 23-30 Dustbin Bags",
-    image: "/images/rocket/md_l.png"
+    image: "/images/rocket/Medium_Load.webp"
   },
   {
     title: "Large",
     meta: "7 Cubic Yards | 5.4 M²",
     note: "1 Builders Skip | 45-55 Dustbin Bags",
-    image: "/images/rocket/larger_l.png"
+    image: "/images/rocket/Large.webp"
   },
   {
     title: "X-Large",
     meta: "10 Cubic Yards | 7.6 M²",
     note: "Small Van Load | 60-80 Dustbin Bags",
-    image: "/images/rocket/xl.png"
+    image: "/images/rocket/X_Large.webp"
   },
   {
     title: "XX-Large",
     meta: "14 Cubic Yards | 10.7 M²",
     note: "2 Builders Skips | 90-100 Dustbin Bags",
-    image: "/images/rocket/xxl.png"
+    image: "/images/rocket/XX_Large.webp"
   }
 ];
